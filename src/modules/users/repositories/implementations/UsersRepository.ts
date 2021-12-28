ebd3b308aeb4ebb84970c0c388d0b17aea5c738b
claudio nkumbula@gmail.com <klaudio.kumbula@gmail.com>
2021-12-28 18:11:42 +0200

    first commit

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,3 +1,4 @@
+import { request } from "express";
 import { User } from "../../model/User";
 import { IUsersRepository, ICreateUserDTO } from "../IUsersRepository";
 
@@ -19,23 +20,49 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      created_at: new Date(),
+      updated_at: new Date()
+    });
+
+    this.users.push(user);
+
+    return user;
+
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    
+    const user = this.users.find((user) => user.id === id);
+
+    return user;
+
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    
+    const user = this.users.find((user) => user.email === email);
+
+    return user;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    
+    receivedUser.admin = true;
+    receivedUser.updated_at = new Date();
+
+    return receivedUser;
+
   }
 
   list(): User[] {
-    // Complete aqui
+
+    return this.users;
+    
   }
 }
 
